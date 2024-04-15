2750900fe703801b8010d90113fa0ea84f201451
Tonyrealzy <umehobiarinze2@gmail.com>
2024-04-15 10:36:04 +0100

    My alx-frontend-javascript tasks

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,3 +1,2 @@
-export default function getBudgetObject(income, gdp, capita) {
-  return { income, gdp, capita };
-}
+const getBudgetObject = (income, gdp, capita) => { income, gdp, capita }
+export default getBudgetObject
