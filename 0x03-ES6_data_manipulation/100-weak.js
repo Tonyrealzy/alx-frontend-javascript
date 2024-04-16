38dc0f4a5c4467ee1c78211e69d5480a0267d1c3
Tonyrealzy <umehobiarinze2@gmail.com>
2024-04-16 12:57:31 +0100

    Completed 100% of my tasks

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,21 @@
+export const weakMap = new WeakMap();
+
+export default function queryAPI(endpoint) {
+    if (typeof endpoint !== 'object' || !endpoint.protocol || !endpoint.name) {
+        throw new Error('Invalid endpoint object');
+    }
+
+    let count = weakMap.get(endpoint);
+
+    if (!count) {
+        count = 0;
+    }
+    count += 1;
+
+    if (count >= 5) {
+        throw new Error('Endpoint load is high');
+    }
+
+    weakMap.set(endpoint, count);
+    console.log(`Calling API endpoint: ${endpoint.protocol}: ${endpoint.name}`);
+}
---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,20 @@
+export default function updateStudentGradeByCity(arrayObject, city, newGrades) {
+    if (!Array.isArray(arrayObject)) {
+        return [];
+    }
+    return arrayObject
+    .filter((el) => 
+        el.location === city
+    )
+    .map((el) => {
+        for (const gr of newGrades) {
+            if (!gr.grade) {
+                gr.grade = 'N/A';
+            }
+            if (gr.studentId === el.id) {
+                el.grade = gr.grade;
+            }
+        }
+        return el;
+    });
+}
