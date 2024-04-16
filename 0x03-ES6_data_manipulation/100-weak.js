6f3166c806d6fabafab82899a40074b124e95b2c
Tonyrealzy <umehobiarinze2@gmail.com>
2024-04-16 22:30:45 +0000

    New changes

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -5,23 +5,23 @@ export default function queryAPI(endpoint) {
     throw new Error('Invalid endpoint object');
   }
 
-  let callCount = 0;
+  const callCount = 0;
 
   if (callCount === 0) {
     weakMap.get(endpoint) === callCount;
   }
-  
+
   let count = weakMap.get(endpoint);
 
   if (!count || count === undefined) {
     count = 0;
   }
   count += 1;
-  
+
   if (count >= 5) {
     throw new Error('Endpoint load is high');
   }
-  
+
   weakMap.set(endpoint, count);
 
   return count;
--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -4,7 +4,7 @@ export default function cleanSet(set, startString) {
   if (!(set instanceof Set)) {
     throw new TypeError('set has to be a Set!');
   }
-  
+
   for (const element of set) {
     if (element.startsWith(startString)) {
       const endString = element.slice(startString.length);
