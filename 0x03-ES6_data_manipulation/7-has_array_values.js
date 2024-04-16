a1198a5d6262a50378320a6d08edacb47a07e9eb
Tonyrealzy <umehobiarinze2@gmail.com>
2024-04-16 12:23:56 +0100

    Completed 100% of my tasks

---
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -0,0 +1,6 @@
+export default function hasValuesFromArray(set, array) {
+    if (Array.isArray(array)) {
+        throw new TypeError('Not an array');
+    }
+    return array.every(element => set.has(element));
+}
