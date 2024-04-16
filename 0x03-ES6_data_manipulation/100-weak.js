411d56590e438736c6dde05d480b73eee9223cac
Tonyrealzy <umehobiarinze2@gmail.com>
2024-04-16 23:27:21 +0100

    Editing my project tasks

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -5,12 +5,18 @@ export default function queryAPI(endpoint) {
     throw new Error('Invalid endpoint object');
   }
 
+  let callCount = 0;
+
+  if (callCount === 0) {
+    weakMap.get(endpoint) === callCount;
+  }
+  
   let count = weakMap.get(endpoint);
 
   if (!count || count === undefined) {
     count = 0;
   }
-  count++;
+  count += 1;
   
   if (count >= 5) {
     throw new Error('Endpoint load is high');
